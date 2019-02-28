88b848efea72b47f3ba6d02ba4f9168ae41c3a68
Code Nation 20 <codenation@CN-20.lan>
2019-02-28 14:14:06 +0000

    Updates

---
+++ precache-manifest.edca7d6436f1052c4b06eb4e11be5c2f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "08a57c4877ae33407fbe",
+    "url": "/React-Calculator/static/js/runtime~main.08a57c48.js"
+  },
+  {
+    "revision": "cbf2953858469f666790",
+    "url": "/React-Calculator/static/js/main.cbf29538.chunk.js"
+  },
+  {
+    "revision": "c88c70e5f4ff8bea6fac",
+    "url": "/React-Calculator/static/js/2.c88c70e5.chunk.js"
+  },
+  {
+    "revision": "cbf2953858469f666790",
+    "url": "/React-Calculator/static/css/main.76a62be9.chunk.css"
+  },
+  {
+    "revision": "e49dd56a13280267921787c63bd7b428",
+    "url": "/React-Calculator/index.html"
+  }
+];
