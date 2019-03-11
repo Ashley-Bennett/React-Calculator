07525917e10f86606ad8edc67a444dc5d9b304a7
Ashley Bennett <codenation@CN-20.local>
2019-03-11 15:33:47 +0000

    Updates

---
+++ precache-manifest.1ad4e169a44d39bcdaee4ad764f8485f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "08a57c4877ae33407fbe",
+    "url": "/React-Calculator/static/js/runtime~main.08a57c48.js"
+  },
+  {
+    "revision": "4998ef63f9fa3aff04a8",
+    "url": "/React-Calculator/static/js/main.4998ef63.chunk.js"
+  },
+  {
+    "revision": "c88c70e5f4ff8bea6fac",
+    "url": "/React-Calculator/static/js/2.c88c70e5.chunk.js"
+  },
+  {
+    "revision": "4998ef63f9fa3aff04a8",
+    "url": "/React-Calculator/static/css/main.ac04db2a.chunk.css"
+  },
+  {
+    "revision": "4b5cb455a1b4de327a0d29b028ac1e65",
+    "url": "/React-Calculator/index.html"
+  }
+];
